73a6b66f2957b55845073971d8ce267887a1d222
Jéssica Neri <jess.neri94@gmail.com>
2020-08-18 20:57:23 -0300

    Documentação: listei os produtos e criei uma função interativa

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
 */
 const readline = require("readline");
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 /*
@@ -18,3 +18,33 @@ const rl = readline.createInterface({
 /**
  * Escreva seu código aqui embaixo;
  */
+const chalk = require('chalk');
+
+const listaDeProdutos = [{
+        nome: "melancia",
+        preco: 5,
+        qtdDisponivel: 4
+    }, {
+        nome: "abacaxi",
+        preco: 3,
+        qtdDisponivel: 5
+    },
+    {
+        nome: "banana",
+        preco: 6,
+        qtdDisponivel: 5
+    }
+];
+
+const buscarProduto = (resposta) => {
+    if (resposta === listaDeProdutos.nome) {
+        console.log(`Yay! Temos seu produto ${chalk.green(nome_do_produto)}`)
+    } else {
+        console.log("Infelizmente não temos este produto.");
+    }
+}
+
+rl.question("Qual produto você está procurando?", (resposta) => {
+    console.log(resposta);
+    buscarProduto(resposta);
+});
